5150662a0f472e2104ba0e61d1e5762a0f1e8b80
ronek22 <jronek3010@gmail.com>
2019-03-12 14:51:33 +0100

    Lab3 done

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -7,7 +7,7 @@
 using namespace cv;
 using namespace std;
 
-int moveDetection() {
+int moveDetectionVideo() {
 	VideoCapture stream("robot_no_loop.avi");
 
 
@@ -35,6 +35,53 @@ int moveDetection() {
 		stream.read(secondFrame2);
 		cvtColor(secondFrame2, secondFrame2, COLOR_RGB2GRAY);
 
+		cvtColor(firstFrame, firstFrame, COLOR_RGB2GRAY);
+		cvtColor(secondFrame, secondFrame, COLOR_RGB2GRAY);
+		absdiff(firstFrame, secondFrame, result); // aktualna i poprzednia
+		absdiff(firstFrame2, secondFrame2, result2); // pierwsza klatka - aktualna
+		threshold(result, result, 100, 255, THRESH_BINARY);
+		threshold(result2, result2, 100, 255, THRESH_BINARY);
+
+		imshow("cam2", result);
+		imshow("cam3", result2);
+
+		if (waitKey(30) == 27)
+			break;
+
+	}
+
+	stream.release();
+	destroyAllWindows();
+}
+
+int moveDetectionCamera() {
+	VideoCapture cap(0);
+
+
+	if (!cap.isOpened()) {
+		cout << "Failed to open video capture" << endl;
+		return -1;
+	}
+
+	Mat firstFrame, secondFrame, result;
+	Mat firstFrame2, secondFrame2, result2;
+
+	cap.read(firstFrame2);
+	cvtColor(firstFrame2, firstFrame2, COLOR_RGB2GRAY);
+
+	while (true) {
+		cap >> firstFrame;
+		cap >> secondFrame;
+
+		if (firstFrame.empty() || secondFrame.empty()) {
+			break;
+		}
+
+		imshow("cam1", secondFrame);
+
+		cap.read(secondFrame2);
+		cvtColor(secondFrame2, secondFrame2, COLOR_RGB2GRAY);
+
 
 
 		cvtColor(firstFrame, firstFrame, COLOR_RGB2GRAY);
@@ -52,15 +99,70 @@ int moveDetection() {
 
 	}
 
-	stream.release();
+	cap.release();
 	destroyAllWindows();
 }
 
+int addLogoToCamera() {
+	VideoCapture cap(0);
+	if (!cap.isOpened()) {
+		cout << "Failed to open video capture" << endl;
+		return -1;
+	}
+
+	Mat image = imread("logo.bmp");
+	Mat frame;
+	int size = 70;
+	resize(image, image, Size(size, size));
 
 
+	while (waitKey(15) != '\33') {
+		cap >> frame;
+
+		Mat insetImage(frame, Rect(0, 0, 70, 70));
+		image.copyTo(insetImage);
+		imshow("frame", frame);
+	}
+
+	cap.release();
+	destroyAllWindows();
+
+	return 0;
+}
+
+
+int addLogoToVideo() {
+	VideoCapture stream("robot_no_loop.avi");
+	if (!stream.isOpened()) {
+		cout << "Failed to open video capture" << endl;
+		return -1;
+	}
+
+	Mat image = imread("logo.bmp");
+	Mat frame;
+	int size = 70;
+	resize(image, image, Size(size, size));
+
+
+	while (waitKey(15) != '\33') {
+		stream >> frame;
+
+		Mat insetImage(frame, Rect(0, 0, 70, 70));
+		image.copyTo(insetImage);
+		imshow("frame", frame);
+	}
+
+	stream.release();
+	destroyAllWindows();
+
+	return 0;
+}
 void printMenu() {
 	cout << "MENU\n";
 	cout << "[1] Move detection camera\n";
+	cout << "[2] Move detection video.\n";
+	cout << "[3] Add logo to camera\n";
+	cout << "[4] Add logo to video\n";
 	cout << "[ESC] To Exit\n";
 }
 
@@ -72,7 +174,16 @@ int main() {
 		switch (choice)
 		{
 		case '1':
-			moveDetection();
+			moveDetectionCamera();
+			break;
+		case '2':
+			moveDetectionVideo();
+			break;
+		case '3':
+			addLogoToCamera();
+			break;
+		case '4':
+			addLogoToVideo();
 			break;
 		default:
 			break;
